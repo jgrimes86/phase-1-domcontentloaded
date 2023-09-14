472e44318773aae480314e543db66c048c5c3679
Jim Grimes <jimgrimes86@gmail.com>
2023-09-13 20:38:15 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    let targetText = document.getElementById('text');
+    targetText.textContent = "This is really cool!";
+});
+
